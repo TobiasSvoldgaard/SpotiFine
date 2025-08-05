b97cd2710d96cf7494254891af66643c6c343b99
Svolle <64897616+TobiasSvoldgaard@users.noreply.github.com>
2025-08-05 06:15:08 +0200

    feat: added component to show top artists

---
+++ spotifine/src/app/components/TopArtists.tsx
@@ -0,0 +1,48 @@
+import { JSX, useState } from "react";
+import { artist } from "../types";
+
+type Props = {
+  mostPlayedArtists: artist[];
+};
+
+export default function TopAlbums({ mostPlayedArtists }: Props): JSX.Element {
+  const [showMore, setShowMore] = useState(false);
+  const [numberOfArtistsToShow, setNumberOfArtistsToShow] =
+    useState<number>(10);
+  return (
+    <>
+      <div className="w-[100%] h-100 bg-[#0f9516] rounded-2xl p-8 overflow-y-auto">
+        <h1 className="text-2xl font-bold">Top Artists</h1>
+        {mostPlayedArtists
+          .slice(0, numberOfArtistsToShow)
+          .map((artist, index) => (
+            <div key={index} className="flex justify-between">
+              <span className="w-[70%] truncate">
+                {index + 1}. {artist.name}
+              </span>
+              <span>
+                {artist.timesPlayed}{" "}
+                {artist.timesPlayed === 1 ? "song played" : "songs played"}
+              </span>
+            </div>
+          ))}
+        <br />
+        {mostPlayedArtists.length > 10 && (
+          <p
+            className="text-right underline cursor-pointer mt-2"
+            onClick={() => {
+              if (showMore) {
+                setNumberOfArtistsToShow(10);
+              } else {
+                setNumberOfArtistsToShow(mostPlayedArtists.length);
+              }
+              setShowMore(!showMore);
+            }}
+          >
+            {showMore ? "Show less" : "Show more"}
+          </p>
+        )}
+      </div>
+    </>
+  );
+}
