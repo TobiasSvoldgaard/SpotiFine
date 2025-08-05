7af85e2bfb72836fdf0933ff9cc63e778a787a79
Svolle <64897616+TobiasSvoldgaard@users.noreply.github.com>
2025-08-05 06:12:55 +0200

    feat: added separate file for types

---
+++ spotifine/src/app/types.ts
@@ -0,0 +1,65 @@
+export type userStats = {
+  totalSongsListenedTo: number;
+};
+
+export type media = {
+  ts: string;
+  platform: string;
+  ms_played: number;
+  conn_country: string;
+  ip_addr: string;
+  master_metadata_track_name: string;
+  master_metadata_album_artist_name: string;
+  master_metadata_album_album_name: string;
+  spotify_track_uri: string;
+  episode_name: string;
+  episode_show_name: string;
+  spotify_episode_uri: string;
+  audiobook_title: string;
+  audiobook_uri: string;
+  audiobook_chapter_uri: string;
+  audiobook_chapter_title: string;
+  reason_start: string;
+  reason_end: string;
+  shuffle: boolean;
+  skipped: boolean;
+  offline: boolean;
+  offline_timestamp: number;
+  incognito_mode: boolean;
+};
+
+export type song = {
+  title: string;
+  artist: string;
+  album: string;
+  timesPlayed: number;
+};
+
+export type artist = {
+  name: string;
+  timesPlayed: number;
+};
+
+export type album = {
+  title: string;
+  artist: string;
+  timesPlayed: number;
+};
+
+export type podcast = {
+  name: string;
+  timesPlayed: number;
+};
+
+export type statistics = {
+  totalSongsPlayed: number;
+  totalPodcastEpisodesPlayed: number;
+  totalAudiobooksPlayed: number;
+  totalSongListeningTime: number;
+  totalPodcastListeningTime: number;
+  totalAudiobookListeningTime: number;
+  mostPlayedSongs: song[];
+  mostPlayedArtists: artist[];
+  mostPlayedAlbums: album[];
+  mostPlayedPodcasts: podcast[];
+};
