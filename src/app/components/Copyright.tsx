5cd3269a285ada38d1a97c887bcec07ec64ea90c
Svolle <64897616+TobiasSvoldgaard@users.noreply.github.com>
2025-08-07 11:07:34 +0200

    fix: made copyright attribution colour consistent across devices

--- src/app/components/Copyright.tsx
+++ src/app/components/Copyright.tsx
@@ -3,7 +3,7 @@ import { JSX } from "react";
 export default function TopSongs(): JSX.Element {
   return (
     <>
-      <div className="absolute bottom-0 right-0 flex items-center text-neutral-400 pr-2 pb-2">
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex items-center text-neutral-400 pr-2 pb-2">
         <p>&copy; Tobias Svoldgaard</p>
         <a href="https://github.com/TobiasSvoldgaard/SpotiFine" target="_blank">
           <svg
@@ -11,10 +11,10 @@ export default function TopSongs(): JSX.Element {
             viewBox="0 0 98 96"
             width="18"
             height="18"
+            fill="#a1a1a1"
             className="github-icon ml-2 my-auto cursor-pointer text-neutral-400"
           >
             <path
-              fill="currentcolor"
               d="M49 .6C21.9.6 0 22.5 0 49.6c0 21.7 14.1 40.1 33.7 46.6 
         2.5.5 3.4-1.1 3.4-2.4 0-1.2 0-5.2-.1-9.4-13.7 3-16.6-5.9-16.6-5.9-2.3-5.8-5.6-7.3-5.6-7.3
         -4.6-3.2.3-3.1.3-3.1 5.1.4 7.8 5.2 7.8 5.2 4.5 7.8 11.7 5.6 14.6 4.3.5-3.2 1.7-5.6 3.1-6.8
